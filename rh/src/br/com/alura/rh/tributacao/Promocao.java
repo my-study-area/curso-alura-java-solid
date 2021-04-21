a3116c9ea46d7dbf3b99192bc7e252088d3cb5cc
Adriano Avelino <adriano-st777@hotmail.com>
2021-04-21 19:32:44 -0300

    adiciona exemplo mal uso Interface Segregation Principle

---
+++ rh/src/br/com/alura/rh/tributacao/Promocao.java
@@ -0,0 +1,30 @@
+package br.com.alura.rh.tributacao;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Promocao implements Reajuste {
+	
+	private BigDecimal valor;
+	private LocalDate data;
+	
+	public Promocao(BigDecimal valor, LocalDate data) {
+		this.valor = valor;
+		this.data = data;
+	}
+	
+	@Override
+	public BigDecimal valor() {
+		return valor;
+	}
+	@Override
+	public LocalDate data() {
+		return data;
+	}
+
+	@Override
+	public BigDecimal valorDoImpostoDeRenda() {
+		return valor.multiply(new BigDecimal("0.1"));
+	}
+
+}
---
+++ rh/src/br/com/alura/rh/tributacao/Reajuste.java
@@ -0,0 +1,12 @@
+package br.com.alura.rh.tributacao;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public interface Reajuste {
+	
+	BigDecimal valor();
+	LocalDate data();
+	BigDecimal valorDoImpostoDeRenda();
+
+}
