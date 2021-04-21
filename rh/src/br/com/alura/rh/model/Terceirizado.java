922876bd9116d956bb02214d52c5c0cb3f37f8ca
Adriano Avelino <adriano-st777@hotmail.com>
2021-04-20 21:57:08 -0300

    adiciona correção da herança

--- rh/src/br/com/alura/rh/model/Terceirizado.java
+++ rh/src/br/com/alura/rh/model/Terceirizado.java
@@ -2,17 +2,38 @@ package br.com.alura.rh.model;
 
 import java.math.BigDecimal;
 
-public class Terceirizado extends Funcionario {
+public class Terceirizado {
 
-	public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario) {
-		super(nome, cpf, cargo, salario);
+	private DadosPessoais dadosPessoais;
+	private String empresa;
+
+	public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario, String empresa) {
+		this.dadosPessoais = new DadosPessoais(nome, cpf, cargo, salario);
+		this.empresa = empresa;
 	}
 	
-	@Override
-	public void atualizarSalario(BigDecimal novoSalario) {
+	public String getEmpresa() {
+		return empresa;
 	}
 
-	@Override
-	public void promover(Cargo novoCargo) {
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
 	}
+
+	public String getNome() {
+		return dadosPessoais.getNome();
+	}
+
+	public String getCpf() {
+		return dadosPessoais.getCpf();
+	}
+
+	public Cargo getCargo() {
+		return dadosPessoais.getCargo();
+	}
+
+	public BigDecimal getSalario() {
+		return dadosPessoais.getSalario();
+	}
+	
 }
